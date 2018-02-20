0af3d948d0741daf1350a75b57a19a28263fb714
Aezore <silverdisc@gmail.com>
2018-02-20 14:00:50 +0100

    coloramed

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-"""[Autoranging program for resistor reading and profiling]
-"""
+"""[Autoranging program for resistor reading and profiling]"""
 #########################################
 # IMPORTS
 #########################################
-
+import logging
 import RPi.GPIO as GPIO
+from colorama import init, Fore, Style
 import Adafruit_ADS1x15
 
-import logging
 logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+                    format=Style.BRIGHT + '%(asctime)s - %(levelname)s - %(message)s')
 
 adc = Adafruit_ADS1x15.ADS1115()            # Creates the instance
 
@@ -39,6 +38,11 @@ RANGE_SCALE_LIST_STR = ("/100K",            # Range labels
 ADC_VOLTAGE_CLAMP = 5.8  # Maximum voltage clamped at adc input
 ADC_MAX_SAMPLE_POINTS = 30860
 ADC_CALIBRATION_FLOOR = 0.042
+VOLTAGE_ADC_FLOOR = 400
+VOLTAGE_ADC_CEILING = 30400
+
+OPEN_CIRCUIT_DBG = Fore.LIGHTRED_EX + 'OPEN CIRCUIT!!'
+SHORT_CIRCUIT_DBG = Fore.LIGHTRED_EX + 'SHORT CIRCUIT!!'
 
 
 #########################################
@@ -49,12 +53,13 @@ ADC_CALIBRATION_FLOOR = 0.042
 def adc_init():
     """[Initializes the ADC settings and mode]
     """
-
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(True)
-    GPIO.setup(MUX_PINS, MUX_PINS_IO)  # Sets the pins 17 and 4 as OUTPUTS
-
-    logging.debug('ADC Initialized.')
+    try:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(True)
+        GPIO.setup(MUX_PINS, MUX_PINS_IO)  # Sets the pins 17 and 4 as OUTPUTS
+        logging.debug(Fore.LIGHTGREEN_EX + 'ADC Initialized OK.')
+    except:
+        logging.debug(Fore.LIGHTRED_EX + 'ERROR ADC INIT - NOT CONNECTED?')
 
 
 def adc_set_range(output_channel):
@@ -72,7 +77,7 @@ def adc_reset_range():
     """[RESETS THE ADC RANGE OUTPUT TO THE HIGHEST 249K ohm value]"""
     GPIO.output(MUX_PINS, RESET_MUX)
 
-    logging.debug('ADC Range MUX RESET.')
+    logging.debug(Fore.LIGHTCYAN_EX 'ADC Range MUX RESET.')
 
 
 def adc_read_average():
@@ -124,6 +129,7 @@ def adc_voltage_conversion(adc_sample_read):
         [int] -- [the sample value converted to resistance according to the scale]
     """
     resistor_value = adc_sample_read * (ADC_VOLTAGE_CLAMP/ADC_MAX_SAMPLE_POINTS) - ADC_CALIBRATION_FLOOR
+    logging.debug('adc_voltage_conv - ADCSAMPLE: {}, RESISTOR: {}'.format(adc_sample_read, resistor_value))
     return resistor_value
 
 
@@ -136,10 +142,12 @@ def adc_resistor_read():
     adc_reset_range()
     adc_sample_read, adc_sample_scale = adc_autorange()
 
-    if adc_sample_read > 30500 and adc_sample_scale == HIGH_IMPEDANCE:
-        return("Open circuit!")
-    elif adc_sample_read < 400 and adc_sample_scale == LOW_IMPEDANCE:
-        return("Short to GND!")
+    if adc_sample_read > VOLTAGE_ADC_CEILING and adc_sample_scale == HIGH_IMPEDANCE:
+        logging.debug(OPEN_CIRCUIT_DBG)
+        return OPEN_CIRCUIT_DBG
+    elif adc_sample_read < VOLTAGE_ADC_FLOOR and adc_sample_scale == LOW_IMPEDANCE:
+        logging.debug(SHORT_CIRCUIT_DBG)
+        return SHORT_CIRCUIT_DBG
     else:
         resistor_value = adc_voltage_conversion(sample=adc_sample_read)
         return resistor_value, adc_sample_scale
@@ -149,13 +157,11 @@ def main():
     """[Main entry point]"""
     logging.debug('Main Entry Point')
     adc_init()
+    init()  # Colorama init
 
     while True:
-        read_pin = adc_resistor_read()
-
-        print("DEBUG: Raw ADC: {0:>6} ADCRANGE: {1}".format(read_pin[0],
-                                                            read_pin[1]
-                                                            ))
+        read_pin, scale = adc_resistor_read()
+        print(read_pin, scale)
         input()
 
 
